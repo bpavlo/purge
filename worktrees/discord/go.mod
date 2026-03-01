641f4b0e478a35d1b021d5e5e0cbb736c6009744
Pavlo B <git@bpavlo.com>
2026-02-28 23:57:02 -0500

    Remove worktrees/ directory (duplicate development artifacts)

--- worktrees/discord/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/pavlo/purge
-
-go 1.25.5
-
-require golang.org/x/time v0.14.0
--- worktrees/foundation/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/pavlo/purge/cmd"
-
-func main() {
-	cmd.Execute()
-}
